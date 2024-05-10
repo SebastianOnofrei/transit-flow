5cdccacf342eecaf5cfba55e02ceaf49fc04768b
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2024-05-11 01:42:47 +0300

    feat: created header common component

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import "./Header.css";
+import logo from "../../../assets/logo.png";
+import icon1 from "../../../assets/headericon1.png";
+import icon2 from "../../../assets/headericon2.png";
+import icon3 from "../../../assets/headericon3.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  const headerItems = [
+    {
+      icon: icon1,
+      text1: "Mon - Sat 9.00 - 18.00",
+      text2: "Sunday Closed",
+    },
+    {
+      icon: icon2,
+      text1: "Email",
+      text2: "contact@logistics.com",
+    },
+    {
+      icon: icon3,
+      text1: "Call Us",
+      text2: "(00) 112 365 489",
+    },
+  ];
+  return (
+    <header>
+      <img src={logo} alt="transit flow company logo" className="logo" />
+      <div className="header__wrapper">
+        {headerItems.map((item) => {
+          return (
+            <div className="header__wrapper__item">
+              <img src={item.icon} alt="" />
+              <div className="header__wrapper__item-info">
+                <p>{item.text1}</p>
+                <p>{item.text2}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
